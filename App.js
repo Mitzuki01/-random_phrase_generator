4c458d88dc0d103cf56a24fa2ac8a5bf611c415f
Alunos <aluno.etec@etecEmbu.com.br>
2023-04-12 21:37:40 -0300

    apk que gera 10 frases aleatórias

--- App.js
+++ App.js
@@ -1,11 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Profiler, useState } from 'react';
+import { StyleSheet, Text, View ,TextInput, TouchableOpacity  } from 'react-native';
 
 export default function App() {
+
+    const[frase,setFrase]=useState('');
+
+    function radNumber(){
+      const number = Math.floor(Math.random()*10)+1;
+      //setFrase(number);
+      switch (number){
+        case 1: setFrase("Oi, feliz natal");
+        break
+        case 2: setFrase("Oi,feliz ano novo");
+        break
+        case 3: setFrase("Oi, feliz aniversario");
+        break 
+        case 4: setFrase("Páscoa ta ai hem");
+        break
+        case 5: setFrase("quero minha casa");
+        break
+        case 6: setFrase("meta de vida? virar dev sênior");
+        break
+        case 7: setFrase("prof, que atividade easy");
+        break
+        case 8: setFrase("posso sair mais cedo?");
+        break
+        case 9 : setFrase("qual seu maior sonho?");
+        break
+        case 10 : setFrase("cabooooooo numero 10");
+      }
+      
+    } 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>
+          {frase}
+      </Text>
+      <TouchableOpacity style={styles.button} onPress={radNumber}>
+        <Text>
+          Gerar Frase Aleatória
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -17,4 +53,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button:{
+    backgroundColor:"blue",
+    color:"#FFF",
+    borderRadius:9,
+    height:25
+  }
 });
